d08958ec2ef402336518c871002a657f223b148e
Paul <kiramishima@outlook.com>
2020-02-14 01:48:52 -0600

    update

---
+++ HackerRank/SuperReducedString/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let rec superReducedString (s:string): string =
+    let str = Seq.toList s
+    if str.Length = 0 then
+        s
+    elif str.Length > 0 then
+        match str.Head then
+        |> x = str.Tail.Head -> String.concat("", str.Tail.Tail)
+        |> _ -> ""
+        ""
+
+[<EntryPoint>]
+let main argv =
+    // let s = Console.ReadLine()
+    let s = "aaabccddd"
+    superReducedString s
+    |> Console.WriteLine
+    0 // return an integer exit code
