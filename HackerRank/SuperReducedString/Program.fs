e3577953c554db46f7fd597e1ab3e25997c144f9
Paul <kiramishima@outlook.com>
2020-02-17 00:31:10 -0600

    test

--- HackerRank/SuperReducedString/Program.fs
+++ HackerRank/SuperReducedString/Program.fs
@@ -1,16 +1,32 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+// open System.Text.RegularExpressions
 
-let rec superReducedString (s:string): string =
-    let str = Seq.toList s
+let rec superReducedString (s:String) =
+    let str = List.ofSeq s
+    printf "%A" str
     if str.Length = 0 then
         s
     elif str.Length > 0 then
-        match str.Head then
-        |> x = str.Tail.Head -> String.concat("", str.Tail.Tail)
-        |> _ -> ""
-        ""
+        let s2 = match str with
+        | x::xs -> 
+            printf "%A" x
+            printf "%A" xs
+            // let s2 = String.concat "" str
+            // superReducedString s2
+            s
+        | [] -> s
+        s2
+
+
+(*let rec superReducedString (s:string): string =
+    let rx = Regex(@"\b(?<word>\w+)(?=<word>)\b", RegexOptions.Compiled)
+    let matches = rx.Matches(s)
+    if matches.Count = 0 then
+        s
+    else
+        superReducedString(rx.Replace(s, ""))*)
 
 [<EntryPoint>]
 let main argv =
