0cef77a5f9c7083fc75484369dc001ba17327f19
kiramishima <kiramishima@outlook.com>
2020-03-07 13:47:10 -0600

    Ejercicio completado

--- HackerRank/SockMerchant/Program.fs
+++ HackerRank/SockMerchant/Program.fs
@@ -2,14 +2,22 @@
 
 open System
 
-let sockMerchant (n:int) (ar: int[]) =
+let parse (s: string) =
+    match (System.Int32.TryParse(s)) with
+    | (true, value) ->  Ok value
+    | (false, _) -> Error "Invalid int"
 
-    0
+let sockMerchant (n:int) (ar: int[]) =
+    let r = ar |> Array.groupBy (fun x -> x) |> Array.map (fun (x, y) -> y)
+    let mutable x = 0
+    for i in 0..r.Length-1 do
+        x <- ((r.[i].Length - (r.[i].Length%2) ) / 2) + x
+    x
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    let n = 9
-    let ar = [|10; 20; 20; 10; 10; 30; 50; 10; 20|]
+    // let(n, m) = Console.ReadLine() |> parse
+    let n = int <| Console.ReadLine()
+    let ar = Console.ReadLine().Split(' ') |> Array.map (fun x -> x |> Convert.ToInt32)
     sockMerchant n ar |> Console.WriteLine
     0 // return an integer exit code
