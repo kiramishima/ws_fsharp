e4e98382393159ef196a1f35676456a65b75e0dd
kiramishima <kiramishima@outlook.com>
2020-03-09 00:05:07 -0600

    Ejercicio iniciado

--- HackerRank/SockMerchant/Program.fs
+++ HackerRank/SockMerchant/Program.fs
@@ -2,13 +2,8 @@
 
 open System
 
-let parse (s: string) =
-    match (System.Int32.TryParse(s)) with
-    | (true, value) ->  Ok value
-    | (false, _) -> Error "Invalid int"
-
 let sockMerchant (n:int) (ar: int[]) =
-    let r = ar |> Array.groupBy (fun x -> x) |> Array.map (fun (x, y) -> y)
+    let r = ar |> Array.groupBy id |> Array.map (fun (x, y) -> y)
     let mutable x = 0
     for i in 0..r.Length-1 do
         x <- ((r.[i].Length - (r.[i].Length%2) ) / 2) + x
@@ -16,7 +11,6 @@ let sockMerchant (n:int) (ar: int[]) =
 
 [<EntryPoint>]
 let main argv =
-    // let(n, m) = Console.ReadLine() |> parse
     let n = int <| Console.ReadLine()
     let ar = Console.ReadLine().Split(' ') |> Array.map (fun x -> x |> Convert.ToInt32)
     sockMerchant n ar |> Console.WriteLine
