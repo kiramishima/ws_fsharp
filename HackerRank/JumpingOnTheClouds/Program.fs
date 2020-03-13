3758b98432bae6b19d380e46a5ccede3ed7bce8d
KiraMishima / <kiramishima@outlook.com>
2020-03-12 20:16:44 -0600

    Nuevo ejercicio

--- HackerRank/JumpingOnTheClouds/Program.fs
+++ HackerRank/JumpingOnTheClouds/Program.fs
@@ -14,13 +14,39 @@ let main argv =
     // Saltos = 1 o 2
     // 0 - 2 - 4 - 6
     // 0 - 2 - 3 - 4 - 6
+    let n = c.Length
     let mutable sum : int list = []
     //let mutable sum2 = 0
-    for i in 0..c.Length-2 do
-        printfn "%i - %i" c.[i] c.[i+1]
-        if c.[i] = 0 && c.[i+1] = 0 then
-            sum <- i::sum
-        elif c.[i] = 1 && c.[i+1] = 0 then
-            sum <- i+1::sum
+    (*let rec operation (index: int) (head: int) (tail: int List) =
+        if index =< n then
+            if head=0 && head=tail.Head then
+                sum <- index + 1 :: sum
+                operation (index + 1) tail.Head tail.Tail
+            elif head=0 && head=tail.Tail.Head then
+                sum <- index + 2 :: sum
+                operation (index + 2) tail.Head tail.Tail
+        elif index > n then
+            List.*)
+    let rec jumps (index: int) (x: int[]) =
+        let cx = x |> Array.toList
+        match cx with
+        | [] -> 0
+        | [a] -> 
+            printf "+++++++++\n %A | %A \n++++++++++" a []
+            jumps (index + 2) ([] |> Seq.toArray)
+        | a::ax -> 
+            printf "+++++++++\n %A | %A \n++++++++++" a ax
+            jumps (index + 2) (ax.Tail |> Seq.toArray)
+        
+        | a::ax when a = ax.Head && a = ax.Tail.Head ->
+            sum <- (index + 2) :: sum
+            jumps (index + 2) (ax.Tail |> Seq.toArray)
+        | a::ax when a = ax.Head ->
+            sum <- (index + 1) :: sum
+            jumps (index + 1) (ax.Tail |> Seq.toArray)
+
+    let res = jumps 0 c
+    Console.WriteLine res
+
     printf "%A" sum
     0 // return an integer exit code
